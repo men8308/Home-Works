06510373ca35f640ea8540c38ec3483d3f2d35e5
Смольянов Николай Александрович <ybrjkfq83@mail.ru>
2023-02-02 06:57:28 +0200

    added a task and its solution

---
+++ Seminar2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая выводит случайное число из отрезка [10; 99] и показывает наибольшую цифру числа.
+int number = new Random() .Next(10, 100);
+
+int number1 = number/10;
+int number2 = number%10;
+
+Console.WriteLine($"number = {number}");
+if (number1 > number2)
+{
+    Console.WriteLine($"number1 = {number1} больше number2 = {number2}");
+}
+else if (number2 >number1)
+{
+Console.WriteLine($"number2 = {number2} больше number1 = {number1}");
+}
+else 
+{
+    Console.WriteLine($"number1 = {number1} равно number2 = {number2}");
+}
