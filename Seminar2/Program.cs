2a3cc6b937b24d99b15072fd274e07a8dc016984
Смольянов Николай Александрович <ybrjkfq83@mail.ru>
2023-02-02 13:34:06 +0200

    added a task3 and its solution

--- Seminar2/Program.cs
+++ Seminar2/Program.cs
@@ -21,11 +21,37 @@
 
 
 //Напишите программу, которая выводит случайное трехзначное число и удаляет вторую цифру этого числа
-System.Console.Clear();
-int number = new Random() .Next(100 ,1000);
+// System.Console.Clear();
+// int number = new Random() .Next(100 ,1000);
 
-int number1 = number/100;
-int number3 = number%10;
+// int number1 = number/100;
+// int number3 = number%10;
  
- System.Console.WriteLine($"number = {number}");
-Console.WriteLine($"{number1}{number3}");
+//  System.Console.WriteLine($"number = {number}");
+// Console.WriteLine($"{number1}{number3}");
+
+
+// Напишите программу, которая будет принимать на вход два числа и выводить, является ли второе число кратным превому
+// Если число 2 кратно 1, то выводим "кратно". Если не кратно то выводим остаток от деления.
+
+System.Console.Clear();
+
+System.Console.WriteLine("Enter number 1");
+string strnumber1 = Console.ReadLine();
+int number1 = Convert.ToInt32(strnumber1);
+
+System.Console.WriteLine("Enter number 2");
+string strnumber2 = Console.ReadLine();
+int number2 = Convert.ToInt32(strnumber2);
+
+int result = number1 % number2;
+
+if  (result == 0)
+{
+    System.Console.WriteLine("Кратно");
+}
+
+else 
+{
+    System.Console.WriteLine($"Остаток от деления = {result}");
+}
