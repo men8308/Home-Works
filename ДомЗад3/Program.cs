477c62b7dfa2349326508d6bfb039617728ce325
Смольянов Николай Александрович <ybrjkfq83@mail.ru>
2023-02-08 15:21:52 +0200

    Написал решение задачи№21

--- ДомЗад3/Program.cs
+++ ДомЗад3/Program.cs
@@ -5,9 +5,12 @@
 // 23432 -> даConsole.WriteLine("Введите число: ");
 
 
-Console.Clear();
+/*Console.Clear();
+
 Console.Write("Введите 5-ти значное число: ");
+
 string number = Console.ReadLine();
+
 int len = number.Length;
 
 if (len == 5)
@@ -24,4 +27,35 @@ if (len == 5)
 else
 {
     Console.WriteLine($"ОШИБКА: {number} - Это не 5-ти значное число!");
+}*/
+
+
+
+
+
+// Задача 21
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+
+Console.Clear();
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
 }
+int x1 = ReadInt("Введите координату X первой точки: ");
+int y1 = ReadInt("Введите координату Y первой точки: ");
+int z1 = ReadInt("Введите координату Z первой точки: ");
+int x2 = ReadInt("Введите координату X второй точки: ");
+int y2 = ReadInt("Введите координату Y второй точки: ");
+int z2 = ReadInt("Введите координату Z второй точки: ");
+
+int A = x2 - x1;
+int B = y2 - y1;
+int C = z1 - z2;
+
+double length = Math.Sqrt(A * A + B * B + C * C);
+Console.WriteLine($"Длинна отрезка {length}");
