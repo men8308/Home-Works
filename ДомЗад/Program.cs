9fde1f59b658d8c05af7118013b86fb6ed997a9a
Смольянов Николай Александрович <ybrjkfq83@mail.ru>
2023-02-08 15:31:25 +0200

    Добавил решения д.з. из семинара№3

--- ДомЗад/Program.cs
+++ ДомЗад/Program.cs
@@ -138,36 +138,123 @@
 // 6 -> да
 // 7 -> да
 // 1 -> нет
-System.Console.Clear();//Чистим экран терминала
-System.Console.Write("enter a number from 1 to 7 ");//вводим число от 1 до 7
-string strnumber = Console.ReadLine();//вводим переменную strnumber1 и считываем в неё введеные значения как строку
-int number = Convert.ToInt32(strnumber);//вводим переменную number1 и конвертируем в неё цифру из strnumber1
-int result;//вводим переменную result
-if (number == 1)
+// System.Console.Clear();//Чистим экран терминала
+// System.Console.Write("enter a number from 1 to 7 ");//вводим число от 1 до 7
+// string strnumber = Console.ReadLine();//вводим переменную strnumber1 и считываем в неё введеные значения как строку
+// int number = Convert.ToInt32(strnumber);//вводим переменную number1 и конвертируем в неё цифру из strnumber1
+// int result;//вводим переменную result
+// if (number == 1)
+// {
+//     System.Console.Write("It's a Monday, a working day");
+// }
+// else if (number == 2)
+// {
+//     System.Console.Write("It's a Tuesday, a working day");
+// }
+// else if (number == 3)
+// {
+//     System.Console.Write("It's a Wednesday, a working day");
+// }
+// else if (number == 4)
+// {
+//     System.Console.Write("It's a Thursday, a working day");
+// }
+// else if (number == 5)
+// {
+//     System.Console.Write("It's a Friday, a working day");
+// }
+// else if (number == 6)
+// {
+//     System.Console.Write("It's a Saturday, a weekend");
+// }
+// // else
+// {
+//     System.Console.Write("It's a Sanday, a weekend");
+// }
+
+
+
+
+
+// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> даConsole.WriteLine("Введите число: ");
+
+
+/*Console.Clear();
+
+Console.Write("Введите 5-ти значное число: ");
+
+string number = Console.ReadLine();
+
+int len = number.Length;
+
+if (len == 5)
 {
-    System.Console.Write("It's a Monday, a working day");
-}
-else if (number == 2)
-{
-    System.Console.Write("It's a Tuesday, a working day");
-}
-else if (number == 3)
-{
-    System.Console.Write("It's a Wednesday, a working day");
-}
-else if (number == 4)
-{
-    System.Console.Write("It's a Thursday, a working day");
-}
-else if (number == 5)
-{
-    System.Console.Write("It's a Friday, a working day");
-}
-else if (number == 6)
-{
-    System.Console.Write("It's a Saturday, a weekend");
+    if (number[0] == number[4] && number[1] == number[3])
+    {
+        Console.WriteLine($"{number} - Палиндром");
+    }
+    else
+    {
+        Console.WriteLine($"{number} - НЕ палиндром");
+    }
 }
 else
 {
-    System.Console.Write("It's a Sanday, a weekend");
+    Console.WriteLine($"ОШИБКА: {number} - Это не 5-ти значное число!");
+}*/
+
+
+
+
+
+// Задача 21
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+
+/*Console.Clear();
+
+int ReadInt(string message)// Функция ввода сообщения
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int x1 = ReadInt("Введите координату X первой точки: ");
+int y1 = ReadInt("Введите координату Y первой точки: ");
+int z1 = ReadInt("Введите координату Z первой точки: ");
+int x2 = ReadInt("Введите координату X второй точки: ");
+int y2 = ReadInt("Введите координату Y второй точки: ");
+int z2 = ReadInt("Введите координату Z второй точки: ");
+
+int A = x2 - x1;
+int B = y2 - y1;
+int C = z1 - z2;
+
+double length = Math.Sqrt(A * A + B * B + C * C); // Надо разобраться как результат округлить до сотых!!!
+Console.WriteLine($"Длинна отрезка {length}");*/
+
+
+
+
+
+// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+int ReadInt(string message)// Функция ввода сообщения
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int number = ReadInt("Введите число N: ");
+
+for (int i = 1; i <= number; i++)
+{ 
+    Console.Write($"{i*i*i} ");
 }
