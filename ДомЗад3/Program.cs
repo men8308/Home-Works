81e5242ab0d1582b26a7babd6ed213c3187ae5d4
Смольянов Николай Александрович <ybrjkfq83@mail.ru>
2023-02-08 14:02:19 +0200

    Написал решение задачи№19

--- ДомЗад3/Program.cs
+++ ДомЗад3/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> даConsole.WriteLine("Введите число: ");
+
+
+Console.Clear();
+Console.Write("Введите 5-ти значное число: ");
+string number = Console.ReadLine();
+int len = number.Length;
+
+if (len == 5)
+{
+    if (number[0] == number[4] && number[1] == number[3])
+    {
+        Console.WriteLine($"{number} - Палиндром");
+    }
+    else
+    {
+        Console.WriteLine($"{number} - НЕ палиндром");
+    }
+}
+else
+{
+    Console.WriteLine($"ОШИБКА: {number} - Это не 5-ти значное число!");
+}
