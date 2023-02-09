7b25a14edbb0c5df09b3d32d96af52285a4922a2
Смольянов Николай Александрович <ybrjkfq83@mail.ru>
2023-02-09 10:55:40 +0200

    Добавил решения д.з. из семинара№4

---
+++ ДомЗад4/Program.cs
@@ -0,0 +1,93 @@
+﻿// Задача 25: (branch task_1)
+// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+/*Console.Clear();
+int A = ReadInt("Введите число A: ");
+int stepen = ReadInt("Введите число B: ");
+ToDegree(A, stepen);
+
+
+// Функция возведения в степень
+void ToDegree(int a, int b)
+{
+    int result = 1;
+    for (int i = 1; i <= b; i++)
+    {
+        result = result * a;
+    }
+    Console.WriteLine(result);
+}
+
+// Функция ввода
+int ReadInt(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}*/
+
+
+
+
+
+// Задача 27: (branch task_2)
+// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+/*Console.Clear();
+int number = ReadInt("Введите число: ");
+int len = NumberLen(number);
+SumNumbers(number, len);
+
+
+// Функция ввода
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+// Функция подсчета цифр в числе
+int NumberLen(int a)
+{
+    int index = 0;
+    while (a > 0)
+    {
+        a /= 10;
+        index++;
+    }
+    return index;
+}
+
+// Функция вывода суммы цифр в числе
+void SumNumbers(int n, int len)
+{
+    int sum = 0;
+    for (int i = 1; i <= len; i++)
+    {
+        sum += n % 10;
+        n /= 10;
+    }
+    Console.WriteLine(sum);
+}*/
+
+
+
+
+
+//Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+Console.Clear();
+int[] array = new int[8];
+
+ for (int i = 0; i < 8; i++)
+    {
+        array[i] = new Random().Next(i+1);
+        Console.Write(array[i] + "; ");
+    }
+Console.WriteLine();
