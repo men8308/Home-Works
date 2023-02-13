7d041019d41c13d9350db5256d8e57d3f4c07717
Смольянов Николай Александрович <ybrjkfq83@mail.ru>
2023-02-13 10:11:18 +0200

    Добавил решение задачи №36 из семинара№5

--- ДомЗад5/Program.cs
+++ ДомЗад5/Program.cs
@@ -3,6 +3,7 @@
 
 Console.Clear(); //Чистим окно терминала
 
+/*
 Console.Write("Enter the size of the array : "); //Просим пользователя ввести размер массива
 
 int size = Convert.ToInt32(Console.ReadLine()); //Преобразовывем из строки в натуральное число
@@ -12,20 +13,15 @@ Array(numbers); // Заполняем наш массив
 Console.Write("This is our array : "); //Выводим в консоль наш заполненный массив 
 PrintArray(numbers);//Выводим в консоль наш заполненный массив
 
-/*
-Считаем количество чётных чисел
-*/
+// Считаем количество чётных чисел
 int count = 0;
 for (int i = 0; i < numbers.Length; i++)
 if (numbers[i] % 2 == 0)
 count++;
 
-//
 Console.Write($"In our array {count} even numbers from {numbers.Length} available");
 
-/*
-Метод заполнения массива
-*/
+// Метод заполнения массива
 void Array(int[] numbers)
 {
     for(int i = 0; i < numbers.Length; i++)
@@ -34,9 +30,8 @@ void Array(int[] numbers)
     }
 }
 
-/*
 Метод вычисления чётных чисел
-*/
+
 void PrintArray(int[] numbers)
 {
     Console.Write("[ ");
@@ -44,6 +39,46 @@ void PrintArray(int[] numbers)
     {
         Console.Write(numbers[i] + " ");
     }
+    Console.Write("]");
+    Console.WriteLine();
+}
+*/
+
+
+
+
+
+// Задача 36: Задайте одномерный массив, заполненный случайными числами.
+// Найдите сумму элементов, стоящих на нечётных позициях.
+
+
+Console.Write("Enter the size of the array : ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
+Array(numbers);
+Console.Write("This is our array : ");
+PrintArray(numbers);
+int sum = 0;
+
+for (int i = 0; i < numbers.Length; i+=2)
+    sum = sum + numbers[i];
+
+    Console.WriteLine($"Sum of elements in odd positions = {sum}");
+
+void Array(int[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = new Random().Next(1,10);
+        }
+}
+void PrintArray(int[] numbers)
+{
+    Console.Write("[ ");
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            Console.Write(numbers[i] + " ");
+        }
     Console.Write("]");
     Console.WriteLine();
 }
