898033f0d21cfc162069405737140d678434f867
Смольянов Николай Александрович <ybrjkfq83@mail.ru>
2023-02-20 04:59:08 +0200

    Добавил решение домашнего задания №50

--- ДомЗад7/Program.cs
+++ ДомЗад7/Program.cs
@@ -1,41 +1,91 @@
 ﻿// Задача 47.Задайте двумерный массив размером m×n,
 // заполненный случайными вещественными числами.
 
-int rows = ReadInt("Enter Rows: ");
-int colums = ReadInt("Введите количество столбцов: ");
-double[,] numbers = new double[rows, colums];
-FillArray2D(numbers);
-PrintArray2D(numbers);
-// Заполнение массива рандомными вещественными числами
-void FillArray2D(double[,] array)
-{
-for (int i = 0; i < array.GetLength(0); i++)
-{
-for (int j = 0; j < array.GetLength(1); j++)
-{
-array[i, j] = new Random().Next(-99, 99) / 10.0;
-}
-}
-}
-// Функция вывода массива в терминал
-void PrintArray2D(double[,] array)
-{
-for (int i = 0; i < array.GetLength(0); i++)
-{
-for (int j = 0; j < array.GetLength(1); j++)
-{
-Console.Write(array[i, j] + " ");
-}
-Console.WriteLine();
-}
-Console.WriteLine();
-}
-// Функция ввода
-int ReadInt(string message)
-{
-Console.Write(message);
-return Convert.ToInt32(Console.ReadLine());
-}
+
+Console.Clear();
+
+// int rows = ReadInt("Enter Rows: ");
+// int colums = ReadInt("Enter Columns: ");
+// double[,] numbers = new double[rows, colums];
+// FillArray2D(numbers);
+// PrintArray2D(numbers);
+// // Заполнение массива рандомными вещественными числами
+// void FillArray2D(double[,] array)
+// {
+// for (int i = 0; i < array.GetLength(0); i++)
+// {
+// for (int j = 0; j < array.GetLength(1); j++)
+// {
+// array[i, j] = new Random().Next(-99, 100) / 10.0;
+// }
+// }
+// }
+// // Функция вывода массива в терминал
+// void PrintArray2D(double[,] array)
+// {
+// for (int i = 0; i < array.GetLength(0); i++)
+// {
+// for (int j = 0; j < array.GetLength(1); j++)
+// {
+// Console.Write(array[i, j] + " ");
+// }
+// Console.WriteLine();
+// }
+// Console.WriteLine();
+// }
+// // Функция ввода
+// int ReadInt(string message)
+// {
+// Console.Write(message);
+// return Convert.ToInt32(Console.ReadLine());
+// }
+
+
+
+
+
+// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Во вводе первая цифра - номер строки, вторая - столбца.
+
+
+// int rows = ReadInt("Enter Rows's index: ");
+// int colums = ReadInt("Enter Columns's endex: ");
+// int[,] numbers = new int[9, 9];
+// FillArray2D(numbers);
+// PrintArray2D(numbers);
+// if (rows < numbers.GetLength(0) && colums < numbers.GetLength(1)) Console.WriteLine(numbers[rows, colums]);
+// else Console.WriteLine($"{rows},{colums} -> there is no such number in the array");
+// // Заполнение массива рандомными числами от 1 до 9
+// void FillArray2D(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             array[i, j] = new Random().Next(1, 10);
+//         }
+//     }
+// }
+// // Функция вывода массива в терминал
+// void PrintArray2D(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             Console.Write(array[i, j] + " ");
+//         }
+//         Console.WriteLine();
+//     }
+//     Console.WriteLine();
+// }
+// // Функция ввода
+// int ReadInt(string message)
+// {
+//     Console.Write(message);
+//     return Convert.ToInt32(Console.ReadLine());
+// }
 
 
 
