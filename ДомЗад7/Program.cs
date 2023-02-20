c12c587e65962eefa646991adc4623bfc0735dac
Смольянов Николай Александрович <ybrjkfq83@mail.ru>
2023-02-20 11:11:41 +0200

    Добавил решение домашнего задания №52

--- ДомЗад7/Program.cs
+++ ДомЗад7/Program.cs
@@ -91,3 +91,107 @@ Console.Clear();
 
 
 
+// Задача 52. Задайте двумерный массив из целых чисел.
+// Найдите среднее арифметическое элементов в каждом столбце.
+
+// int rows = ReadInt("Enter Rows: ");
+// int colums = ReadInt("Enter Columns: ");
+// int[,] numbers = new int[rows, colums];
+
+// FillArray2D(numbers);
+// PrintArray2D(numbers);
+// ReadInt(numbers);
+
+// // Заполнение массива рандомными числами от 1 до 9
+// void FillArray2D(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             array[i, j] = new Random().Next(1, 10);
+//         }
+//     }
+// }
+
+// // Функция вывода массива в терминал
+// void PrintArray2D(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             Console.Write(array[i, j] + " ");
+//         }
+//         Console.WriteLine();
+//     }
+//     Console.WriteLine();
+// }
+
+// // Функция ввода
+// int ReadInt(string message)
+// {
+//     Console.Write(message);
+//     return Convert.ToInt32(Console.ReadLine());
+// }
+
+
+
+int rows = 3;
+int colums = 4;
+int[,] numbers = new int[rows, colums];
+
+FillArray2D(numbers);
+PrintArray2D(numbers);
+
+double[] avgNumbers = new double[numbers.GetLength(1)];
+for (int i = 0; i < numbers.GetLength(1); i++)
+{
+    double result1 = 0.0;
+    for (int j = 0; j < numbers.GetLength(0); j++)
+    {
+        result1 += numbers[j, i];
+    }
+    avgNumbers[i] = Math.Round(result1 / numbers.GetLength(0), 2);
+}
+PrintArray(avgNumbers);
+// Заполнение массива рандомными вещественными числами
+void FillArray2D(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+// Функция вывода двумерного массива в терминал
+void PrintArray2D(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+// Функция вывода массива в терминал
+void PrintArray(double[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+
+
+
+
+
+
