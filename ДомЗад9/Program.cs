8774eb496b518e089f203283ea385fb94bcd0fa7
Смольянов Николай Александрович <ybrjkfq83@mail.ru>
2023-03-02 07:37:07 +0200

    Добавил решение задачи №68

--- ДомЗад9/Program.cs
+++ ДомЗад9/Program.cs
@@ -38,6 +38,7 @@ NumberCounterReverse(number);
 // Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
 
 
+/*
 int m = InputNumbers("Enter natural number m: ");
 int n = InputNumbers("Enter natural number n: ");
 int temp = m;
@@ -67,4 +68,51 @@ int InputNumbers(string input)
   int output = Convert.ToInt32(Console.ReadLine());
   return output;
 }
+*/
 
+
+
+
+
+
+
+
+
+
+
+
+// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии.
+// Даны два неотрицательных числа m и n.
+
+
+Console.Write("Enter positive number M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter positive number N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+AkkermanFunction(m,n);
+
+
+// вызов функции Аккермана
+void AkkermanFunction(int m, int n)
+{
+    Console.Write($"Function Akkerman = {Akkerman(m, n)}"); 
+}
+
+// функция Аккермана
+int Akkerman(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (n == 0 && m > 0)
+    {
+        return Akkerman(m - 1, 1);
+    }
+    else
+    {
+        return (Akkerman(m - 1, Akkerman(m, n - 1)));
+    }
+}
